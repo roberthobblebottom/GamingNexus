b0eafdddc498b74dd2a33b9c10224bd4bbc70957
YANG XI <e0319016@u.nus.edu>
2020-04-22 02:59:06 +0800

    Retrieve Games By Tags Done

--- GamingNexusRws/src/java/ws/restful/resources/ApplicationConfig.java
+++ GamingNexusRws/src/java/ws/restful/resources/ApplicationConfig.java
@@ -24,13 +24,13 @@ public class ApplicationConfig extends Application {
      * out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(ws.restful.resources.CategoryResource.class);
         resources.add(ws.restful.resources.CosFilter.class);
         resources.add(ws.restful.resources.CustomerResource.class);
-        resources.add(ws.restful.resources.ProductResource.class);
-        resources.add(ws.restful.resources.CategoryResource.class);
         resources.add(ws.restful.resources.GameResource.class);
         resources.add(ws.restful.resources.HardwareResource.class);
         resources.add(ws.restful.resources.OtherSoftwareResource.class);
+        resources.add(ws.restful.resources.ProductResource.class);
         resources.add(ws.restful.resources.PromotionResource.class);
         resources.add(ws.restful.resources.TagResource.class);
 
